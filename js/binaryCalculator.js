9b22a345c77389cc3cf22afb43a92741bfd14dc9
GIjoe <gijoe6688@gmail.com>
2022-01-05 01:48:49 +0100

    update 1 js

--- js/binaryCalculator.js
+++ js/binaryCalculator.js
@@ -16,12 +16,9 @@ function clear(){
     res.innerHTML="";
 }
 function eql(){
-    var expr = res.innerHTML;
-    var nums = /(\d+)/g;
-    // Replace all base 2 nums with base 10 equivs
-    expr = expr.replace(nums, function(match) {
+    let expr = res.innerHTML;
+    expr = expr.replace(/(\d+)/g, function(match) {
         return parseInt(match, 2);
     })
-    // eval in base 10 and convert to base 2
     res.innerHTML = eval(expr).toString(2);
 }
